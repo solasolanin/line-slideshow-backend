2925d7566429bd8f221b125fa2ea461276c0bd75
hidetaka <asaokahide@yahoo.co.jp>
2023-11-11 02:55:10 +0900

    賞テーブル作成

--- aws/DynamoDb/deploy-dev.sh
+++ aws/DynamoDb/deploy-dev.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+ENV=dev
+
 aws cloudformation deploy \
     --template-file ./dynamodb.yaml \
-    --stack-name dynamodb-dev \
+    --stack-name dynamodb-$ENV \
     --parameter-overrides \
-        Env=dev
+        Env=$ENV
+
+# 初期値投入
+aws dynamodb put-item \
+    --table-name line-slideshow-dynamodb-contest-$ENV \
+    --item '{
+        "prize_id": {"N": "1"},
+        "prize_name": {"S": "最優秀賞"},
+        "photo_id": {"S": ""},
+        "account_id": {"S":""}
+      }'
+aws dynamodb put-item \
+    --table-name line-slideshow-dynamodb-contest-$ENV \
+    --item '{
+        "prize_id": {"N": "2"},
+        "prize_name": {"S": "新婦賞"},
+        "photo_id": {"S": ""},
+        "account_id": {"S":""}
+      }'
+aws dynamodb put-item \
+    --table-name line-slideshow-dynamodb-contest-$ENV \
+    --item '{
+        "prize_id": {"N": "3"},
+        "prize_name": {"S": "新郎賞"},
+        "photo_id": {"S": ""},
+        "account_id": {"S":""}
+      }'
