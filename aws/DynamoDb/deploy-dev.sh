484e86840aee653a7fffd80b8d71af301eaa20eb
hidetaka <asaokahide@yahoo.co.jp>
2023-11-19 03:28:12 +0900

    スライドショー暫定

--- aws/DynamoDb/deploy-dev.sh
+++ aws/DynamoDb/deploy-dev.sh
@@ -15,7 +15,7 @@ aws dynamodb put-item \
         "prize_id": {"N": "1"},
         "prize_name": {"S": "最優秀賞"},
         "photo_id": {"S": ""},
-        "account_id": {"S":""}
+        "account_name": {"S":""}
       }'
 aws dynamodb put-item \
     --table-name line-slideshow-dynamodb-contest-$ENV \
@@ -23,7 +23,7 @@ aws dynamodb put-item \
         "prize_id": {"N": "2"},
         "prize_name": {"S": "新婦賞"},
         "photo_id": {"S": ""},
-        "account_id": {"S":""}
+        "account_name": {"S":""}
       }'
 aws dynamodb put-item \
     --table-name line-slideshow-dynamodb-contest-$ENV \
@@ -31,5 +31,5 @@ aws dynamodb put-item \
         "prize_id": {"N": "3"},
         "prize_name": {"S": "新郎賞"},
         "photo_id": {"S": ""},
-        "account_id": {"S":""}
+        "account_name": {"S":""}
       }'
