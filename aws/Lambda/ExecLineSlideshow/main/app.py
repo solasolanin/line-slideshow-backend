577f4dbee7246ade205143f07fbef9a036a349f2
hidetaka <asaokahide@yahoo.co.jp>
2023-11-22 20:38:05 +0900

    eventに複数枚含まれるときの対応

--- aws/Lambda/ExecLineSlideshow/main/app.py
+++ aws/Lambda/ExecLineSlideshow/main/app.py
@@ -23,52 +23,58 @@ def lambda_handler(event, context):
         # メッセージのパース
         event_body = json.loads(event['Records'][0]['body'])
 
-        # メッセージ形式の確認
-        if event_body['events'][0]['type'] != "message":
-            return response.not_message()
+        if event_body['events'] is None:
+            raise ValueError('event_body[\'events\'] is None')
 
-        if event_body['events'][0]['message']['type'] != "image":
-            return response.not_image()
-
-        message = event_body['events'][0]['message']
     except Exception as e:
         return response.message_format_error(event)
 
     # トークンインスタンスの初期化
     line_token_info = line_token.LineToken(env)
 
-    # 画像取得
-    message_id = event_body['events'][0]['message']['id']
-    photo_info = photo.Photo(line_token_info.access_token, message_id)
-    if photo_info.code != 200:
-        return response.internal_error(f'photo status code: {photo_info.code}')
-    with open('/tmp/'+photo_info.filename, 'wb') as f:
-        f.write(photo_info.content)
+    for event in event_body['events']:
 
-    # 画像のプレビュー情報取得
-    preview_info = preview.Preview(line_token_info.access_token, message_id)
-    if preview_info.code != 200:
-        response.internal_error(f'preview status code: {preview_info.code}')
-    with open('/tmp/'+preview_info.filename, 'wb') as f:
-        f.write(preview_info.content)
+        # メッセージ形式の確認
+        if event['type'] != "message":
+            continue
 
-    # 画像アップロード
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(f"line-slideshow-s3-{env}")
-    bucket.upload_file('/tmp/' + photo_info.filename,
-                       Key=f"img/{photo_info.filename}")
-    bucket.upload_file('/tmp/'+preview_info.filename,
-                       Key=f"tmb/{preview_info.filename}")
+        if event['message']['type'] != "image":
+            continue
 
-    # アカウント名取得
-    account_info = account.Account(
-        line_token_info.access_token, event_body['events'][0]['source']['userId'])
-    account_name = account_info.account_name
-    account_id = account_info.account_id
+        # 画像取得
+        message_id = event['message']['id']
+        photo_info = photo.Photo(line_token_info.access_token, message_id)
+        if photo_info.code != 200:
+            return response.internal_error(f'photo status code: {photo_info.code}')
+        with open('/tmp/'+photo_info.filename, 'wb') as f:
+            f.write(photo_info.content)
 
-    # メッセージ情報登録
-    set_msg_info(account_id, message_id, account_name,
-                 photo_info.filename, preview_info.filename, photo_info.extension)
+        # 画像のプレビュー情報取得
+        preview_info = preview.Preview(
+            line_token_info.access_token, message_id)
+        if preview_info.code != 200:
+            response.internal_error(
+                f'preview status code: {preview_info.code}')
+        with open('/tmp/'+preview_info.filename, 'wb') as f:
+            f.write(preview_info.content)
+
+        # 画像アップロード
+        s3 = boto3.resource('s3')
+        bucket = s3.Bucket(f"line-slideshow-open-s3-{env}")
+        bucket.upload_file('/tmp/' + photo_info.filename,
+                           Key=f"img/{photo_info.filename}")
+        bucket.upload_file('/tmp/'+preview_info.filename,
+                           Key=f"tmb/{preview_info.filename}")
+
+        # アカウント名取得
+        account_info = account.Account(
+            line_token_info.access_token, event['source']['userId'])
+        account_name = account_info.account_name
+        account_id = account_info.account_id
+
+        # メッセージ情報登録
+        set_msg_info(account_id, message_id, account_name,
+                     photo_info.filename, preview_info.filename, photo_info.extension)
 
     return response.success()
 
